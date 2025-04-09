9402df132af928eeeb030b370ccd2f4033c390b3
angel <angelspsv@gmail.com>
2025-04-09 16:26:43 +0200

    fetch retornar dades usuari fet

--- js_angel/comprador_menu_inici.js
+++ js_angel/comprador_menu_inici.js
@@ -13,7 +13,7 @@ function getCookie(name) {
 document.addEventListener("DOMContentLoaded", function () {
     const buttonsContainer = document.getElementById("buttons-container");
 
-    // Recuperar las cookies
+    //recuperar cookies
     const userEmail = getCookie('user_email');
     const userName = getCookie('user_name');
     const userType = getCookie('user_type');
@@ -26,8 +26,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     
 
-    // Saludo al usuario
-    let nom = userName;  //'Angel'; // debería tomar el nombre real del usuario
+    //salutacio a l'usuari
+    let nom = userName;  
     const nom_usuari = document.querySelector('.username');
 
     if (nom_usuari && nom) {
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
         nom_usuari.textContent = `Hola!`;
     }
 
-    // Crear botones dinámicamente
+    //fem els botons del menu dinamicament
     const buttons = [
         { text: "Les meves dades", link: "dades_comprador.html" },
         { text: "Les meves comandes", link: "comprador_comandes.html" },
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
         buttonsContainer.appendChild(btnElement);
     });
 
-    // Crear input y botón de búsqueda
+    //fem el input y boto de cerca
     const contenidor = document.getElementById('per_input_i_boto');
     const input_text = document.createElement('input');
     input_text.type = 'text';
@@ -80,93 +80,6 @@ document.addEventListener("DOMContentLoaded", function () {
     contenidor.appendChild(boto_cerca);
 });
 
-
-/*
-function getCookie(name) {
-    let value = "; " + document.cookie;
-    let parts = value.split("; " + name + "=");
-    if (parts.length === 2) {
-        let cookieValue = parts.pop().split(";").shift();
-        console.log(`Cookie ${name}:`, cookieValue);  // Esto te ayudará a verificar si la cookie está presente
-        return cookieValue;
-    }
-    console.log(`Cookie ${name} no encontrada`);
-    return null;
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    const buttonsContainer = document.getElementById("buttons-container");
-
-    //recuperem les dades de la cookie
-    const userEmail = getCookie('user_email');
-    const userName = getCookie('user_name');
-    const userSurname = getCookie('user_surname');
-    const userType = getCookie('user_type');
-
-    
-    //salutacio usuari
-    let nom = userName;      //'Angel'; //haura d'agafar el nom real de l'usuari
-    const nom_usuari = document.querySelector('.username');
-    //nom_usuari.textContent = nom.toUpperCase();
-
-
-    if (nom_usuari && nom) {
-        nom_usuari.textContent = nom.toUpperCase();
-    } else {
-        console.error('Problema amb el nom de usuari');
-    }
-
-
-    //fem un array amb el text dels botons i les urls
-    const buttons = [
-        { text: "Les meves dades", link: "dades_comprador.html" },
-        { text: "Les meves comandes", link: "comprador_comandes.html" },
-        { text: "Tancar sessió", link: "tancar_sessio.html" },
-        { imgSrc: "imatges_angel/216477_shopping_cart_icon.png", link: "cistella.html" }
-        // { text: "Cistella", link: "cistella.html" } //en cop del text podem posar l'imatge d'un carreto
-    ];
-
-    //bucle per generar dinamicament els botons
-    buttons.forEach(button => {
-        let btnElement = document.createElement("a");
-        btnElement.href = button.link;
-        btnElement.classList.add("btn");
-
-        if (button.imgSrc) {
-            let imgElement = document.createElement("img");
-            imgElement.src = button.imgSrc;
-            imgElement.alt = "Carrito";
-            imgElement.classList.add("icono-cistella"); // Nueva clase CSS
-            btnElement.appendChild(imgElement);
-        } else {
-            btnElement.textContent = button.text;
-        }
-
-        //inserim els botons al contenidor creat per aquests
-        buttonsContainer.appendChild(btnElement);
-    });
-
-    //faig un contenidor pels dos elements
-    const contenidor = document.getElementById('per_input_i_boto');
-
-    //faig el input i el boto de cerca de productes
-    const input_text = document.createElement('input');
-    input_text.type = 'text';
-    input_text.placeholder = 'Què necessites?';
-    input_text.classList.add("input-text");  // Afegim una classe per CSS
-    contenidor.appendChild(input_text);
-
-    //faig el boto que activara la cerca de articles en la meteixa comarca
-    const boto_cerca = document.createElement('button');
-    boto_cerca.textContent = 'Cercar';
-    boto_cerca.classList.add("btn");  // Reutilitzem el mateix estil dels altres botons
-    boto_cerca.addEventListener('click', function(){
-        //cridar a l'endpoint por cercar productes de la mateixa comarca
-        console.log('està cercant productes...');
-    });
-    contenidor.appendChild(boto_cerca);
-});
-*/
 
 //funcio encara per desenvolupar
 //coneixent qui es el venedor es cridara el endpoint per retornar tots els seus productes
