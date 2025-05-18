3cbc2594ea2566a5b9b6ee979e674f2f2c588768
roc <2023_roc.bigas@iticbcn.cat>
2025-05-18 23:05:46 +0200

    Versió final API

--- api_roc/functions.py
+++ api_roc/functions.py
@@ -11,11 +11,11 @@ def user_schema(user) -> dict:
         "dni": user[1],
         "nom": user[2],
         "cognom": user[3],
-        "correu": user[4],
+        "email": user[4],
         "contrasenya": user[5],
         "telefon": user[6],
         "comarca": user[7],
-        "tipus_usuari": user[8],
+        "tipus_usuaris": bool(user[8]),
         "compte_banc": user[9],
     }
 
@@ -26,8 +26,10 @@ def users_schema(uresult) -> dict:
 # Rep una tupla amb les dades del producte i retorna un diccionari
 def product_schema(product) -> dict:
     return {
+        "id": product[0],
+        "id_venedor": product[1],
         "nom": product[2],
-        "desc": product[3],
+        "descripcio": product[3],
         "preu": product[4],
         "stock": product[5],
         "url_imatge": product[6]
@@ -54,7 +56,7 @@ def read_usuari(id):
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM usuaris WHERE id = %s", (id,))
         usuari = cursor.fetchone()
-
+                
         # Si no existeix l'usuari amb id X executa excepció
         if usuari is None:
             raise HTTPException(status_code=404, detail="No existeix un usuari amb aquesta ID.")
@@ -110,17 +112,77 @@ def read_contrasenya(email):
 
         # Definim la query i l'executem
         cursor = conn.cursor()
-        cursor.execute(f"SELECT contrasenya, tipus_usuaris FROM usuaris WHERE email = '{email}'")
+        cursor.execute(f"SELECT id, contrasenya, tipus_usuaris FROM usuaris WHERE email = '{email}'")
         data = cursor.fetchone()
         
         # Si existeix una contrasenya asociada al mail la retorna, si no, retorna None
         if data is None:
-            return {"password": None, "userType": None}
+            return {"id": None, "password": None, "userType": None}
         else:
-            return {"password": data[0], "userType": data[1]}
+            return {"id":data[0], "password": data[1], "userType": data[2]}
 
     except mysql_connector.Error as e:
         raise HTTPException(status_code=500, detail=f"Error en obtenir la contrasenya: {e}")
+
+    finally:
+        if "conn" in locals() and conn:
+            conn.close()
+        if "tunnel" in locals() and tunnel:
+            tunnel.stop()
+            
+# Aquest mètode obté els productes d'un usuari amb una ID determinada
+def read_producte(id):
+    try:
+        result = get_db_conection()
+
+        if isinstance(result, dict) and result.get("status") == -1:
+            return result
+
+        conn, tunnel = result
+
+        # Fem la query i l'executem
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM productes WHERE id = '{id}'")
+        producte = cursor.fetchone()
+        
+        # Si existeix el producte associat a la id retorna un dict amb les dades, sino retorna none
+        if producte is None:
+            return {"producte": None}
+        else:
+            return product_schema(producte)
+
+    except mysql_connector.Error as e:
+        raise HTTPException(status_code=500, detail=f"Error en obtenir producte: {e}")
+
+    finally:
+        if "conn" in locals() and conn:
+            conn.close()
+        if "tunnel" in locals() and tunnel:
+            tunnel.stop()
+
+# Aquest mètode obté tots els productes de la base de dades
+def read_all_productes():
+    try:
+        result = get_db_conection()
+
+        if isinstance(result, dict) and result.get("status") == -1:
+            return result
+
+        conn, tunnel = result
+
+        # Fem la query i l'executem
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM productes")
+        products = cursor.fetchall()
+        
+        # Si existeixen productes  retorna la llista, sino retorna none
+        if products is None:
+            return {"productes": None}
+        else:
+            return products_schema(products)
+
+    except mysql_connector.Error as e:
+        raise HTTPException(status_code=500, detail=f"Error en obtenir productes: {e}")
 
     finally:
         if "conn" in locals() and conn:
@@ -143,16 +205,14 @@ def read_productes(id):
         cursor.execute(f"SELECT * FROM productes WHERE id_venedor = '{id}'")
         products = cursor.fetchall()
         
-        # Si existeix una contrasenya asociada al mail la retorna, si no retorna None
+        # Si existeixen productes associats a la id retorna la llista, sino retorna none
         if products is None:
             return {"productes": None}
         else:
-            print("AQUIIIIIIIIIIII")
-            print(products_schema(products))
             return products_schema(products)
 
     except mysql_connector.Error as e:
-        raise HTTPException(status_code=500, detail=f"Error en obtenir la contrasenya: {e}")
+        raise HTTPException(status_code=500, detail=f"Error en obtenir productes: {e}")
 
     finally:
         if "conn" in locals() and conn:
@@ -196,7 +256,7 @@ def create_usuari(usuari):
         return {"message": "Nou usuari inserit amb èxit."}
     
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error a la BBDD: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error en crear usuari: {str(e)}")
     
     finally:
         if "conn" in locals() and conn:
@@ -219,13 +279,14 @@ def create_producte(producte):
             
             # Preparem l'insert per la taula
             cursor.execute(
-                "INSERT INTO productes (id_venedor, nom, descripcio, preu, stock) VALUES (%s, %s, %s, %s, %s)",
+                "INSERT INTO productes (id_venedor, nom, descripcio, preu, stock, url_imatge) VALUES (%s, %s, %s, %s, %s, %s)",
                 (
                     producte.id_venedor,
                     producte.nom,
                     producte.descripcio,
                     producte.preu,
                     producte.stock,
+                    producte.url_imatge
                 ),
             )
             
@@ -235,10 +296,156 @@ def create_producte(producte):
             return {"message": "Nou producte inserit amb èxit."}
         
         except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Error a la BBDD: {str(e)}")
+            raise HTTPException(status_code=500, detail=f"Error en crear producte: {str(e)}")
         
         finally:
             if "conn" in locals() and conn:
                 conn.close()
             if "tunnel" in locals() and tunnel:
                 tunnel.stop()
+
+
+# <----- MÈTODES UPDATE ----->
+
+# Aquest mètode actualitza un producte existent a la taula productes
+def update_producte(id, producte):
+    try:
+        result = get_db_conection()
+        
+        if isinstance(result, dict) and result.get("status") == -1:
+            return result
+        
+        conn, tunnel = result
+        
+        # Connectem a la BBDD
+        cursor = conn.cursor()
+                
+        # Preparem l'actualització de la taula
+        cursor.execute(
+            """
+            UPDATE productes
+            SET nom = %s, descripcio = %s, preu = %s, stock = %s, url_imatge = %s
+            WHERE id = %s
+            """,
+            (
+                producte.nom,
+                producte.descripcio,
+                producte.preu,
+                producte.stock,
+                producte.url_imatge,
+                id,
+            ),
+        )
+        
+        # Comprovem si alguna fila ha estat modificada
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=404, detail="Producte no trobat.")
+        
+        # Desem els canvis a la taula/BBDD
+        conn.commit()
+        
+        return {"message": "Producte actualitzat amb èxit."}
+    
+    except mysql_connector.Error as e:
+        raise HTTPException(status_code=500, detail=f"Error en actualitzar el producte: {e}")
+    
+    finally:
+        if "conn" in locals() and conn:
+            conn.close()
+        if "tunnel" in locals() and tunnel:
+            tunnel.stop()
+
+# Aquest mètode actualitza un usuari existent a la taula usuaris
+def update_usuari(id, usuari):
+    try:
+        result = get_db_conection()
+        
+        if isinstance(result, dict) and result.get("status") == -1:
+            return result
+        
+        conn, tunnel = result
+        
+        # Connectem a la BBDD
+        cursor = conn.cursor()
+                
+        # Preparem l'actualització de la taula
+        cursor.execute(
+            """
+            UPDATE usuaris
+            SET dni = %s, 
+                nom = %s, 
+                cognom = %s, 
+                email = %s, 
+                contrasenya = %s, 
+                telefon = %s, 
+                comarca = %s, 
+                tipus_usuaris = %s, 
+                compte_banc = %s
+            WHERE id = %s
+            """,
+            (
+                usuari.dni,
+                usuari.nom,
+                usuari.cognom,
+                usuari.email,
+                usuari.contrasenya,
+                usuari.telefon,
+                usuari.comarca,
+                usuari.tipus_usuaris,
+                usuari.compte_banc,
+                id,
+            ),
+        )
+        
+        # Comprovem si alguna fila ha estat modificada
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=404, detail="Usuari no trobat.")
+        
+        # Desem els canvis a la taula/BBDD
+        conn.commit()
+        
+        return {"message": "Usuari actualitzat amb èxit."}
+    
+    except mysql_connector.Error as e:
+        raise HTTPException(status_code=500, detail=f"Error en actualitzar l'usuari: {e}")
+    
+    finally:
+        if "conn" in locals() and conn:
+            conn.close()
+        if "tunnel" in locals() and tunnel:
+            tunnel.stop()
+
+
+# <----- MÈTODES DELETE ----->
+
+def delete_producte(id):
+    try:
+        result = get_db_conection()
+        
+        if isinstance(result, dict) and result.get("status") == -1:
+            return result
+        
+        conn, tunnel = result
+        
+        # Connectem a la BBDD
+        cursor = conn.cursor()
+        
+        # Preparem l'actualització de la taula
+        cursor.execute(f"DELETE FROM productes WHERE id = '{id}'")
+        
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=404, detail="Producte no trobat.")
+
+        # Desem els canvis a la taula/BBDD
+        conn.commit()
+        
+        return {"message": "Producte eliminat amb èxit."}
+    
+    except mysql_connector.Error as e:
+        raise HTTPException(status_code=500, detail=f"Error en eliminar el producte: {e}")
+    
+    finally:
+        if "conn" in locals() and conn:
+            conn.close()
+        if "tunnel" in locals() and tunnel:
+            tunnel.stop()
--- api_roc/main.py
+++ api_roc/main.py
@@ -18,6 +18,33 @@ app.add_middleware(
 )
 
 
+# <----- BASE MODELS ----->
+
+# Validar les dades abans del POST/insert
+class Usuari(BaseModel):
+    dni: str
+    nom: str
+    cognom: str
+    email: str
+    contrasenya: str
+    telefon: int
+    comarca: str
+    tipus_usuaris: bool
+    compte_banc: str
+    
+# Validar les dades de Producte abans del POST/insert
+class Producte(BaseModel):
+    id_venedor: int
+    nom: str
+    descripcio: str
+    preu: float
+    stock: int
+    url_imatge: str
+
+
+# <----- ENDPOINTS GET ----->
+
+# Endpoint arrel
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -41,24 +68,25 @@ async def retornar_contrasenya(email: str):
     return read_contrasenya(email)
 
 
-# Enpoint per retornar una llista amb tots els productes d'un venedor determinat a partir de la seva ID
+# Enpoint que retorna un producte a partir de la seva ID
+@app.get("/producte/{id}", response_model=dict)
+async def retornar_producte(id: int):
+    return read_producte(id)
+
+
+# Enpoint que retorna una llista amb tots els productes d'un venedor determinat a partir de la seva ID
+@app.get("/productes/", response_model=List[dict])
+async def retornar_productes():
+    return read_all_productes()
+
+
+# Enpoint que retorna una llista amb tots els productes d'un venedor determinat a partir de la seva ID
 @app.get("/productes/{id}", response_model=List[dict])
 async def retornar_productes(id: int):
     return read_productes(id)
 
 
-# Validar les dades abans del POST/insert
-class Usuari(BaseModel):
-    dni: str
-    nom: str
-    cognom: str
-    email: str
-    contrasenya: str
-    telefon: int
-    comarca: str
-    tipus_usuaris: bool
-    compte_banc: str
-
+# <----- ENDPOINTS POST ----->
 
 # Endpoint nou_usuari
 @app.post("/nou_usuari/", response_model=dict)
@@ -66,19 +94,32 @@ async def nou_usuari(usuari: Usuari):
     resultat_insert = create_usuari(usuari)
     return resultat_insert
 
-
-# Validar les dades de Producte abans del POST/insert
-class Producte(BaseModel):
-    id_venedor: int
-    nom: str
-    descripcio: str
-    preu: float
-    stock: int
-    url_imatge: str
-
-
 # Endpoint nou producte (POST/INSERT) a la taula productes
 @app.post("/nou_producte/", response_model=dict)
 async def nou_producte(producte: Producte):
     result = create_producte(producte)
+    return result
+
+
+# <----- ENDPOINTS PUT ----->
+
+# Endpoint per editar producte (PUT/UPDATE) a la taula productes
+@app.put("/editar_producte/{id}", response_model=dict)
+async def editar_producte(id: int, producte: Producte):
+    result = update_producte(id, producte)
+    return result
+
+# Endpoint per editar usuari (PUT/UPDATE) a la taula usuaris
+@app.put("/editar_usuari/{id}", response_model=dict)
+async def editar_usuari(id: int, usuari: Usuari):
+    result = update_usuari(id, usuari)
+    return result
+
+
+# <----- ENDPOINTS DELETE ----->
+
+# Endpoint per eliminar producte (DELETE) a la taula productes
+@app.delete("/eliminar_producte/{id}", response_model=dict)
+async def eliminar_producte(id: int):
+    result = delete_producte(id)
     return result
